1aa2b776be06729bdd0b534619468df1939bf4ea
vetonxhumkar890@gmail.com <vetonxhumkar890@gmail.com>
2023-09-27 23:52:59 +0200

    RemoveDupFromSortArr

--- RemoveDupFromSortedArray/Program.cs
+++ RemoveDupFromSortedArray/Program.cs
@@ -5,11 +5,28 @@
 
         static void Main(string[] args)
         {
+            int[] nums = new int[3];
+            nums[0] = 1;
+            nums[1] = 1;
+            nums[2] = 2;
+            RemoveDuplicates(nums);
         }
 
-        public int RemoveDuplicates(int[] nums)
+        public static int RemoveDuplicates(int[] nums)
         {
-            
+            int counter = 0;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[counter] != nums[i])
+                {
+                    counter++;
+                    nums[counter] = nums[i];
+                }
+            }
+
+
+            return counter+1;
         }
     }
 }

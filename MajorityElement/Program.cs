0985e4c6746f3f125db967a0f146b3c5c2d66236
vetonxhumkar890@gmail.com <vetonxhumkar890@gmail.com>
2023-10-03 16:55:04 +0200

    MajorityElement

---
+++ MajorityElement/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace MajorityElement
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[3];
+            nums[0] = 3;
+            nums[1] = 2;
+            nums[2] = 3;
+            MajorityElement(nums);
+        }
+
+        public static int MajorityElement(int[] nums)
+        {
+            Dictionary<int, int> majorityElem = new Dictionary<int, int>();
+            foreach (int num in nums)
+            {
+                if (!majorityElem.ContainsKey(num))
+                {
+                    majorityElem.Add(num, 1);
+                }
+                else
+                {
+                    majorityElem[num]++;
+                }
+            }
+
+
+            return majorityElem.MaxBy(k => k.Value).Key;
+        }
+
+    }
+}

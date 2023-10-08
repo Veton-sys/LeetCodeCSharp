5d662c211d972a4f59531cffecf26c49ebbb78db
vetonxhumkar890@gmail.com <vetonxhumkar890@gmail.com>
2023-10-08 15:50:37 +0200

    RandomizedSet Hash works better

---
+++ RandomizedSet/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace RandomizedSet
+{
+    public class Program
+    {
+        List<int> numbers = new List<int>();
+        static void Main(string[] args)
+        {
+
+        }
+
+        public bool Insert(int val)
+        {
+            if (numbers.Any(n => n == val)) return false;
+
+            numbers.Add(val);
+            return true;
+        }
+
+        public bool Remove(int val)
+        {
+            if (numbers.Any(n => n == val))
+            {
+                numbers.Remove(val);
+                return true;
+            }
+
+            return false;
+        }
+
+        public int GetRandom()
+        {
+            Random r = new Random();
+
+            return numbers.ElementAt(r.Next(numbers.Count));
+        }
+    }
+}
